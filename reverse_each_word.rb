a72fe594237f13b2c43cbebb3507daefdae1fbbe
Israel Canessa <israellcanessa@gmail.com>
2020-05-18 08:30:03 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 string = "Hello there, and how are you?"
 def reverse_each_word(string)
   string.split.each do |words|
-   print words.reverse
+   print words.reverse.join(" ")
   end
 end
 reverse_each_word(string)
