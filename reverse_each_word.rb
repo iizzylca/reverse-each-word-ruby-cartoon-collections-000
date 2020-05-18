8125016da64410fb61e5f1871b99d80f854a06f4
Israel Canessa <israellcanessa@gmail.com>
2020-05-18 08:36:21 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,6 @@
 
 def reverse_each_word(string)
-my_string= "Hello there, and how are you?"
+string= "Hello there, and how are you?"
   string.split {|word| word.reverse}
 end
 reverse_each_word(string)
